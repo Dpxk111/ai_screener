5074fa4bbb191c7cda6529e7fd840141c8da9ff9
Dpak <dpak2002@gmail.com>
2025-08-23 18:16:03 +0530

    resolving disallowed hosts

--- ai_screener/settings.py
+++ ai_screener/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-h)xj2^3@3otllw#em2+8vzph)+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
